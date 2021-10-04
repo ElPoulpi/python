503c3b5a0f1c1762f60c626f927e36a99ccdd30e
RISDORFER Quentin <55832687+ElPoulpi@users.noreply.github.com>
2021-10-04 15:16:24 +0200

    text

---
+++ base/cmd_linux.py
@@ -0,0 +1,6 @@
+import os
+cmd = 'touch tmp.txt'
+cmd2 = 'sudo su'
+
+os.system(cmd)
+os.system(cmd2)
---
+++ base/hello.py
@@ -0,0 +1,6 @@
+def dire_bonjour(nom):
+    for i in range(3):
+        print("Bonjour,", nom, "!")
+        print("ça va?")
+
+dire_bonjour("Poulpi")
